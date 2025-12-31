fce2d7ec93a91f60f04303c932e1968e38f3441b
unknown <nyibonggeorge01@gmail.com>
2025-12-31 20:54:35 +0300

    Update wsgi.py for gunicorn compatibility and remove wagi.py

--- wsgi.py
+++ wsgi.py
@@ -1,7 +1,5 @@
-from app import create_app
-import os
-# Create the Flask application
-application = create_app()
+from app import app as application
+
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8000))
-    application.run(host='0.0.0.0', port=port, debug=False)
+    port = int(os.environ.get("PORT", 10000))
+    application.run(host='0.0.0.0', port=port)
